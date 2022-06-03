9a4ecd93157fc4c2a37a4ce4bfe77f99c87e1909
geraldwogan <geraldwogan@gmail.com>
2022-06-03 01:02:36 +0100

    Improve formatting of comments to improve readiablilty of challenge titles

--- challenges.py
+++ challenges.py
@@ -1,4 +1,6 @@
-# Capital indexes (2/10)
+############################################
+
+#### Capital Indexes (2/10) ####
 
 # Write a function named capital_indexes. The function takes a single parameter, which is a string. Your function should return a list of all the indexes in the string that have capital letters.
 
--- test_challenges.py
+++ test_challenges.py
@@ -1,12 +1,19 @@
 import pytest
 import challenges as ch
 
-# Capital indexes (2/10)
+############################################
+
+#### Capital Indexes (2/10) ####
 
 # Write a function named capital_indexes. The function takes a single parameter, which is a string. Your function should return a list of all the indexes in the string that have capital letters.
 
 # For example, calling capital_indexes("HeLlO") should return the list [0, 2, 4].
 
-def test_capital_indexes():
-    capitals = ch.capital_indexes("heLLo")
-    assert capitals == [2,3]
+def capital_indexes(string_in):
+    capitals = []
+    for idx, letter in enumerate(string_in):
+        if letter.isupper():
+            capitals.append(idx)
+    return capitals
+
+print(capital_indexes("heLIo"))
