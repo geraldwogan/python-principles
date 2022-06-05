8774239d031cb24da41ed55913976fbe37c46d32
geraldwogan <geraldwogan@gmail.com>
2022-06-05 16:43:32 +0100

    Completed 'Type Check' challenge

--- challenges.py
+++ challenges.py
@@ -78,8 +78,28 @@ print(online_count(statuses))
 # For example, calling random_number() some times might first return 42, then 63, then 1.
 
 import random
+from re import T
 
 def random_number():
     return random.randint(0,100)
 
 print(random_number())
+
+########################################################
+#### Type Check (2/10) ####
+# https://pythonprinciples.com/challenges/Type-check/
+
+# Write a function named only_ints that takes two parameters. Your function should return True if both parameters are integers, and False otherwise.
+
+# For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
+
+def only_ints(in_1, in_2):
+    if isinstance(in_1, int) and isinstance(in_2, int) :
+        return True
+    return False
+
+print(only_ints(1,'r'))
+
+
+
+
