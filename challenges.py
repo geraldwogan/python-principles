cbf3b815d13ff6daf10c67241f99a57390bcefb2
geraldwogan <geraldwogan@gmail.com>
2022-06-09 13:55:36 +0100

    Completed 'Divisible by 3' challenge

--- challenges.py
+++ challenges.py
@@ -210,3 +210,14 @@ def largest_difference(list_in):
     return max(list_in) - min(list_in)
 
 print(largest_difference([-1, 2, -3]))
+
+########################################################
+#### Divisible by 3 (3/10) ####
+# https://pythonprinciples.com/challenges/Divisible-by-3/
+
+# Define a function named div_3 that returns True if its single integer parameter is divisible by 3 and False otherwise.
+
+# For example, div_3(6) is True because 6/3 does not leave any remainder. However div_3(5) is False because 5/3 leaves 2 as a remainder.
+
+def div_3(int_in):
+    return int_in % 3 == 0 
