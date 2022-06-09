13cb5a007f40ad29929137c17bd9e8926517bf8d
geraldwogan <geraldwogan@gmail.com>
2022-06-10 00:33:44 +0100

    Initial completion of 'Tic tac toe input' challenge

--- challenges.py
+++ challenges.py
@@ -223,3 +223,39 @@ def div_3(int_in):
     return int_in % 3 == 0 
 
 print(div_3(6))
+
+########################################################
+#### Tic tac toe input (4/10) ####
+# https://pythonprinciples.com/challenges/Tic-tac-toe-input/
+
+# Here's the backstory for this challenge: imagine you're writing a tic-tac-toe game, where the board looks like this:
+
+# 1|  X | O | X
+#  | -----------
+# 2|    |   |  
+#  | -----------
+# 3|  O |   |
+#  |____________
+#     A   B   C
+
+# The board is represented as a 2D list:
+
+# board = [
+#     ["X", "O", "X"],
+#     [" ", " ", " "],
+#     ["O", " ", " "],
+# ]
+
+# Imagine if your user enters "C1" and you need to see if there's an X or O in that cell on the board. To do so, you need to translate from the string "C1" to row 0 and column 2 so that you can check board[row][column].
+
+# Your task is to write a function that can translate from strings of length 2 to a tuple (row, column). Name your function get_row_col; it should take a single parameter which is a string of length 2 consisting of an uppercase letter and a digit.
+
+# For example, calling get_row_col("A3") should return the tuple (2, 0) because A3 corresponds to the row at index 2 and column at index 0in the board.
+
+def get_row_col(string_in):
+    mappings = {'A':0,'B':1,'C':2}
+    return (int(string_in[-1])-1, mappings[string_in[0]] )
+    
+print(get_row_col("A2"))
+print(get_row_col("B3"))
+print(get_row_col("C1"))
