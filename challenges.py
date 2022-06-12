34bbebd4e4395f281a9e2e8b2886ed6fc38e69a9
geraldwogan <geraldwogan@gmail.com>
2022-06-12 22:34:15 +0100

    Completed 'Writing Short Code' challenge

--- challenges.py
+++ challenges.py
@@ -334,4 +334,18 @@ def triple_and(bool_1, bool_2, bool_3):
 print(triple_and(True, True, True))
 print(triple_and(True, True, False))
 
+########################################################
+#### Writing Short Code (5/10) ####
+# https://pythonprinciples.com/challenges/Writing-short-code/
 
+# Define a function named convert that takes a list of numbers as its only parameter and returns a list of each number converted to a string.
+
+# For example, the call convert([1, 2, 3]) should return ["1", "2", "3"].
+
+# What makes this tricky is that your function body must only contain a single line of code.
+
+def convert(list_in):
+    return [str(item) for item in list_in]
+
+print(convert([1, 2, 3]))
+print(convert([]))
