b6dd723aa4a745b67ab83b29c9cd813a9936f5d7
geraldwogan <geraldwogan@gmail.com>
2022-06-12 18:35:03 +0100

    Add test for 'Up and Down' challenge

--- test_challenges.py
+++ test_challenges.py
@@ -253,3 +253,16 @@ def test_palindrome():
     assert ch.palindrome('abba') == True
     assert ch.palindrome('bob') == True
     assert ch.palindrome('abcd') == False
+
+########################################################
+#### Up and Down (4/10) ####
+# https://pythonprinciples.com/challenges/Up-and-down/
+
+# Define a function named up_down that takes a single number as its parameter. Your function should return a tuple containing two numbers; the first should be one lower than the parameter, and the second should be one higher.
+
+# For example, calling up_down(5) should return (4, 6).
+
+def test_up_down():
+    assert ch.up_down(5) == (4, 6)
+    assert ch.up_down(0) == (-1, 1)
+    assert ch.up_down(100) == (99, 101)
