11a6952359d81d1006fe8bc35777fbc5be13dbda
geraldwogan <geraldwogan@gmail.com>
2022-06-13 00:28:54 +0100

    Add test for 'Custom Zip' challenge

--- challenges.py
+++ challenges.py
@@ -352,6 +352,7 @@ print(convert([]))
 
 ########################################################
 #### Custom Zip (5/10) ####
+# https://pythonprinciples.com/challenges/Custom-zip/
 
 # The built-in zip function "zips" two lists. Write your own implementation of this function.
 
@@ -373,3 +374,4 @@ def zap(a, b):
     return [(a[i], b[i]) for i in range(len(a))]
 
 print(zap([0, 1, 2, 3], [5, 6, 7, 8]))
+print(zap([], []))
--- test_challenges.py
+++ test_challenges.py
@@ -321,3 +321,29 @@ def test_convert():
     assert ch.convert([1, 2, 3]) == ['1', '2', '3']
     assert ch.convert([1]) == ['1']
     assert ch.convert([]) == []
+
+########################################################
+#### Custom Zip (5/10) ####
+# https://pythonprinciples.com/challenges/Custom-zip/
+
+# The built-in zip function "zips" two lists. Write your own implementation of this function.
+
+# Define a function named zap. The function takes two parameters, a and b. These are lists.
+
+# Your function should return a list of tuples. Each tuple should contain one item from the a list and one from b.
+
+# You may assume a and b have equal lengths.
+
+# If you don't get it, think of a zipper.
+
+# For example:
+# zap([0, 1, 2, 3], [5, 6, 7, 8])
+
+# Should return:
+# [(0, 5), (1, 6), (2, 7), (3, 8)]
+
+def test_zap():
+    assert ch.zap([0, 1, 2, 3], [5, 6, 7, 8]) == [(0, 5), (1, 6), (2, 7), (3, 8)]
+    assert ch.zap([0], [1]) == [(0, 1)]
+    assert ch.zap([], []) == []
+
