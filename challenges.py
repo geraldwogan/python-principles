0fea33df77ab1a3846662db687048e6b9e06b3db
geraldwogan <geraldwogan@gmail.com>
2022-06-12 18:34:26 +0100

    Completed 'Up and Down' challenge

--- challenges.py
+++ challenges.py
@@ -276,3 +276,16 @@ def palindrome(string_in):
 print(palindrome('abba'))
 print(palindrome('bob'))
 print(palindrome('abcd'))
+
+########################################################
+#### Up and Down (4/10) ####
+# https://pythonprinciples.com/challenges/Up-and-down/
+
+# Define a function named up_down that takes a single number as its parameter. Your function should return a tuple containing two numbers; the first should be one lower than the parameter, and the second should be one higher.
+
+# For example, calling up_down(5) should return (4, 6).
+
+def up_down(int_in):
+    return (int_in-1, int_in+1)
+
+print(up_down(5))
