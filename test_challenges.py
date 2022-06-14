87003070e3dea1d8d8e24a35bb83cbea89821a9f
geraldwogan <geraldwogan@gmail.com>
2022-06-14 20:08:29 +0100

    Add test for 'List xor' challenge

--- test_challenges.py
+++ test_challenges.py
@@ -401,3 +401,25 @@ def test_validate():
         return 'missing return'
     return True""") == True
 
+########################################################
+#### List xor (5/10) ####
+# https://pythonprinciples.com/challenges/List-xor/
+
+# Define a function named list_xor. Your function should take three parameters: n, list1 and list2.
+
+# Your function must return whether n is exclusively in list1 or list2.
+
+# In other words, if n is in both lists or in none of the lists, return False. If n is in only one of the lists, return True.
+
+# For example:
+
+# list_xor(1, [1, 2, 3], [4, 5, 6]) == True
+# list_xor(1, [0, 2, 3], [1, 5, 6]) == True
+# list_xor(1, [1, 2, 3], [1, 5, 6]) == False
+# list_xor(1, [0, 0, 0], [4, 5, 6]) == False
+
+def test_list_xor():
+    assert ch.list_xor(1, [1, 2, 3], [4, 5, 6]) == True
+    assert ch.list_xor(1, [0, 2, 3], [1, 5, 6]) == True
+    assert ch.list_xor(1, [1, 2, 3], [1, 5, 6]) == False
+    assert ch.list_xor(1, [0, 0, 0], [4, 5, 6]) == False
